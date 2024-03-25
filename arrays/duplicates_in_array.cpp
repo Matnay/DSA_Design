4f149a1bdeb83e810de87d40a4ea33b1105829d9
Matnay <f20170487@goa.bits-pilani.ac.in>
2024-03-24 23:50:00 -0400

    modified structure

--- arrays/duplicates_in_array.cpp
+++ arrays/duplicates_in_array.cpp
@@ -1,18 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<int> findDuplicates(vector<int>& nums) {
+    vector<int> findDuplicates(vector<int> &nums)
+    {
         std::vector<int> duplicates;
         int size_c = nums.size();
-        for(int i=0;i<size_c;i++)
+        for (int i = 0; i < size_c; i++)
         {
-            if(nums[abs(nums[i])-1]<0)
+            if (nums[abs(nums[i]) - 1] < 0)
             {
                 duplicates.push_back(abs(nums[i]));
             }
-            nums[abs(nums[i])-1]*=-1;
+
+            // Use in place multiplication with negative number to mark duplicate numbers
+            nums[abs(nums[i]) - 1] *= -1;
         }
         return duplicates;
     }
@@ -21,11 +25,11 @@ public:
 int main()
 {
     Solution sol;
-    vector<int> nums = {4,3,2,7,8,2,3,1};
+    vector<int> nums = {4, 3, 2, 7, 8, 2, 3, 1};
     vector<int> ans;
-    
+
     ans = sol.findDuplicates(nums);
 
-    for(auto num:ans)
-        cout<<num<<endl;
+    for (auto num : ans)
+        cout << num << endl;
 }
