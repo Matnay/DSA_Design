b62deef7d2857d06d4770d580e00c473a5466ed6
Matnay <f20170487@goa.bits-pilani.ac.in>
2024-03-24 23:46:41 -0400

    day1

---
+++ duplicates_in_array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        std::vector<int> duplicates;
+        int size_c = nums.size();
+        for(int i=0;i<size_c;i++)
+        {
+            if(nums[abs(nums[i])-1]<0)
+            {
+                duplicates.push_back(abs(nums[i]));
+            }
+            nums[abs(nums[i])-1]*=-1;
+        }
+        return duplicates;
+    }
+};
+
+int main()
+{
+    Solution sol;
+    vector<int> nums = {4,3,2,7,8,2,3,1};
+    vector<int> ans;
+    
+    ans = sol.findDuplicates(nums);
+
+    for(auto num:ans)
+        cout<<num<<endl;
+}
